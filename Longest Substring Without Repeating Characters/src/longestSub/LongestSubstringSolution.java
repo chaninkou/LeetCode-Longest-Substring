b133ae7cb17d3921a660e31ca60d5c0fc8f98a7c
chaninkou <michaelkoulove@yahoo.com>
2019-06-09 00:46:40 -0700

    Finished this coding question using hashmap.

--- Longest Substring Without Repeating Characters/src/longestSub/LongestSubstringSolution.java
+++ Longest Substring Without Repeating Characters/src/longestSub/LongestSubstringSolution.java
@@ -1,31 +1,49 @@
 package longestSub;
 
+import java.util.HashMap;
+
 public class LongestSubstringSolution {
 	public static void main(String[] args){
-		// To be Continue after my interview
+		// Input
+		String input = "abcdefgabc";
+		
+		System.out.println("Input: " + input);
+		
+		int solution = lengthOfLongestSubstring(input);
+		
+		// solution
+		System.out.println("Solution: " + solution);
+		
 	}
-	
-	class Solution {
-	    public int lengthOfLongestSubstring(String s) {
-	        if(s.length() == 0){
-	            return 0;
-	        }
-	        
-	        HashMap<Character, Integer> map = new HashMap<Character,Integer>();
-	        
-	        int max = 0;
-	        
-	        for(int i = 0, j = 0; i < s.length(); i++){
-	            if(map.containsKey(s.charAt(i))){
-	                
-	            }
-	            map.put(s.charAt(i),i);
-	            max = Math.max(max,i-j+1);
-	        }
-	        
-	        return max;
-	    }
+
+	public static int lengthOfLongestSubstring(String s) {
+		// If its empty, then 0 longest substring
+		if (s.length() == 0) {
+			return 0;
+		}
+
+		// Hash map with string as keys and their position as values
+		HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+
+		int max = 0;
+
+		for (int i = 0, j = 0; i < s.length(); i++) {
+			if (map.containsKey(s.charAt(i))) {
+				j = Math.max(j, map.get(s.charAt(i)) + 1);
+			}
+			
+			// Putting the character into the hashmap
+			map.put(s.charAt(i), i);
+			
+			// Getting the max by comparing the current and max
+			max = Math.max(max, i - j + 1);
+//			System.out.println("Current: "+ s.charAt(i));
+//			System.out.println("max: "+ max);
+//			System.out.println("Next: " + (i - j + 1));
+		}
+
+		return max;
 	}
-	
+
 }
  
