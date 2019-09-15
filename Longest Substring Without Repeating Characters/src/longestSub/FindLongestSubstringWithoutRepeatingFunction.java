6095f165e3584a0e95dc2d7f870e56c11a0c6f2a
chaninkou <michaelkoulove@yahoo.com>
2019-09-15 01:54:00 -0700

    Great problem. Able to find the longest substring without duplicates.

--- Longest Substring Without Repeating Characters/src/longestSub/FindLongestSubstringWithoutRepeatingFunction.java
+++ Longest Substring Without Repeating Characters/src/longestSub/FindLongestSubstringWithoutRepeatingFunction.java
@@ -4,36 +4,33 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class FindLongestSubstringWithoutRepeatingFunction {
-	public static int lengthOfLongestSubstring(String s) {
-		// If its empty, then 0 longest substring
-		if (s.length() == 0) {
-			return 0;
-		}
-
-		// Hash map with string as keys and their position as values
-		Map<Character, Integer> map = new HashMap<>();
-
-		int max = 0;
-
-		for (int i = 0, j = 0; i < s.length(); i++) {
-			if (map.containsKey(s.charAt(i))) {
-				int currentRepeat = map.get(s.charAt(i)) + 1;
-				j = Math.max(j, currentRepeat);
-			}
-			
-			// Putting the character into the hashmap
-			map.put(s.charAt(i), i);
-			
-			// Getting the max by comparing the current and max
-			int current = i - j + 1;
-			max = Math.max(max, current);
-//			System.out.println("Current: "+ s.charAt(i));
-//			System.out.println("max: "+ max);
-//			System.out.println("Next: " + (i - j + 1));
-		}
-
-		return max;
-	}
+	public int lengthOfLongestSubstring(String s) {
+        // If the string is empty, then there is no substring
+        if(s.length() == 0){
+            return 0;
+        }
+        
+        // Storing each character as key and the index of the character as value
+        HashMap<Character, Integer> map = new HashMap<>();
+        
+        // The longest substring
+        int max = 0;
+        
+        for(int index = 0, numbersOfRepeatedCharacter = 0; index < s.length(); index++){
+            // If the character is already in the map, add one to the value of the key
+            if(map.containsKey(s.charAt(index))){
+                numbersOfRepeatedCharacter = Math.max(numbersOfRepeatedCharacter, map.get(s.charAt(index)) + 1);
+            }
+            
+            // Put the key and value in the map
+            map.put(s.charAt(index), index);
+            
+            // Getting the max sub string
+            max = Math.max(max, index - numbersOfRepeatedCharacter + 1);
+        }
+        
+        return max;
+    }
 
 }
  
